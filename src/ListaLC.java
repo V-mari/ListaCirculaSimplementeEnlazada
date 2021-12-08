86e4cff52e0c9eef4543b387978c1a468bd1677a
V-mari <violettrujillo565@gmail.com>
2021-12-07 20:32:43 -0400

    lcse

--- src/ListaLC.java
+++ src/ListaLC.java
@@ -7,9 +7,12 @@ public class ListaLC {
 
 
     }
+    //metodo para saber cuando la lista esta vacia
     public boolean estaVacia(){
         return ultimo == null;
     }
+    //metodo para insertar Nodos
+
     public ListaLC insertar(int elemento){
         NodoLC nuevo= new NodoLC(elemento);
         if (ultimo!= null){
@@ -22,12 +25,42 @@ public class ListaLC {
     // Metodo para mostrar la lista
     public void mostrarLista(){
         NodoLC auxiliar = ultimo.siguiente;
-        String cadena="";
+        String cadena=" ";
         do{
-cadena=cadena + "[" + auxiliar.dato+"]->";
+cadena=cadena + "["+auxiliar.dato+"]->";
+auxiliar=auxiliar.siguiente;
         }while(auxiliar!=ultimo.siguiente);
         JOptionPane.showMessageDialog(null, cadena,
                 "Mostrando La Lista Circular", JOptionPane.INFORMATION_MESSAGE);
 
+    }
+    //metodo para eliminar el Nodo de la lista Circular
+    public  boolean eliminar(int elemento){
+
+        NodoLC actual;
+        boolean encontrado =false;
+        actual = ultimo;
+        while(actual.siguiente!=ultimo && !encontrado) {
+            encontrado = (actual.siguiente.dato==elemento);
+            if(!encontrado){
+                actual = actual.siguiente;
+            }
+
+        }
+        encontrado=(actual.siguiente.dato==elemento);
+        if (encontrado){
+            NodoLC auxiliar=actual.siguiente;
+            if (ultimo==ultimo.siguiente){
+                ultimo=null;
+            }else{
+                if (auxiliar==ultimo){
+                    ultimo=actual;
+                }
+                actual.siguiente=auxiliar.siguiente;
+            }
+            auxiliar=null;
+
+        }
+        return  encontrado=true;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,78 +1,61 @@
 import javax.swing.*;
 
 public class Main {
-    public static  void main(String[] args){
+    public static void main(String[] args) {
 
-    ListaDoble listita = new ListaDoble();
-    int opcion = 0, elemento;
-    do{
-        try{
-            opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
-                    "1. Agregar un Nodo al Inicio\n"
-            +"2. Agregar uN Nodo al final\n"
-            +"3. Mostrar la Lista de Inicio a Fin\n"
-            +"4. Mostrar la lista de Fin a Inicio\n"
-            +"5. Eliminar un nodo del inicio\n"
-            +"6. Eliminar un nodo del final\n"
-            +"7. Salir\n"
-            +"¡Que deseas hacer?","Menu de opciones",
-                    JOptionPane.INFORMATION_MESSAGE));
-            switch (opcion){
-                case 1 :
-                    elemento = Integer.parseInt(JOptionPane.showInputDialog(null,
-                            "Ingrese el elemento del Nodo", "Agregando nodo al inicio",
+        ListaLC listita = new ListaLC();
+        int opcion = 0, elemento;
+        boolean eliminado=false;
+        do {
+            try {
+                opcion = Integer.parseInt(JOptionPane.showInputDialog(null,
+                        "1. Agregar un Nodo a la Lista Circular\n"
+                                + "2. Eliminar un nodo de la Lista Circular \n"
+                                + "3. Mostrar los datos de la Lista Circular\n"
+                                + "4. Salir\n"
+                                + "¡Que deseas hacer?", "Menu de opciones",
+                        JOptionPane.INFORMATION_MESSAGE));
+                switch (opcion) {
+                    case 1:
+                        elemento = Integer.parseInt(JOptionPane.showInputDialog(null,
+                                "Ingrese el elemento del Nodo", "Agregando Nodo a la Lista Circular",
+                                JOptionPane.INFORMATION_MESSAGE));
+                        listita.insertar(elemento);
+                        break;
+                    case 2:
+                        elemento = Integer.parseInt(JOptionPane.showInputDialog(null,
+                              "Ingrese al elemento del nodo a Eliminar", "Eliminando Nodo de la Lista Circular",
                             JOptionPane.INFORMATION_MESSAGE));
-                    listita.agregarAlInicio(elemento);
-                    break;
-                case 2 :
-                    elemento = Integer.parseInt(JOptionPane.showInputDialog(null,
-                            "Ingrese al elemento del nodo", "Agregando Nodo al Final",
-                            JOptionPane.INFORMATION_MESSAGE));
-                    listita.agregarAlFinal(elemento);
-                    break;
-                case 3 :
-                    if (!listita.estaVacia()){
-                        listita.mostrarListaInicioFin();
-                    } else {
-                        JOptionPane.showMessageDialog(null, "No hay nodos aun",
-                                "lista Vacia", JOptionPane.INFORMATION_MESSAGE);
-                    }
-                    break;
-                case 4 :
-                    if (!listita.estaVacia()){
-                        listita.mostrarListaFinInicio();
-                    }else{
-                        JOptionPane.showMessageDialog(null, "No hay nodos aun",
-                                "Lista Vacia", JOptionPane.INFORMATION_MESSAGE);
-                    }
-                    break;
-                case 5 :
-                    if (!listita.estaVacia()){
-                        elemento = listita.eliminarDelInicio();
-                        JOptionPane.showMessageDialog(null, "El elemento eliminado es " + elemento,
-                                "Eliminando nodo del final", JOptionPane.INFORMATION_MESSAGE);
-                    } else{
-                        JOptionPane.showMessageDialog(null, "No hay nodos aun",
-                                "Lista Vacia", JOptionPane.INFORMATION_MESSAGE);
-                    }
-                    break;
-                case 6 :
-                    if (!listita.estaVacia()){
-                        elemento = listita.eliminarDelFinal();
-                        JOptionPane.showMessageDialog(null, "El elemento eliminado es " + elemento,
-                                "Eliminando Nodo del Final", JOptionPane.INFORMATION_MESSAGE);
-                    } else{
-                        JOptionPane.showMessageDialog(null, "No hay Nodos Aun",
-                                "Lista Vacia", JOptionPane.INFORMATION_MESSAGE);
-                    }
-                    break;
-                case 7 :
-                    JOptionPane.showMessageDialog(null, "La opcion no esta en el Menú",
-                            "Incorrecto", JOptionPane.INFORMATION_MESSAGE);
+                        eliminado=listita.eliminar(elemento);
+                        if (eliminado){
+                            JOptionPane.showMessageDialog(null, "El Elemento Eliminado es "+ elemento,
+                                    "Eliminando Nodos", JOptionPane.INFORMATION_MESSAGE);
+                        }else{
+                            JOptionPane.showMessageDialog(null, "El Elemento "+elemento+" No esta en la Lista",
+                                    "Elemento no encontrado", JOptionPane.INFORMATION_MESSAGE);
+                        }
+                        break;
+                    case 3:
+                        if (!listita.estaVacia()) {
+                            listita.mostrarLista();
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Aun no hay nodos",
+                                    "lista Vacia", JOptionPane.INFORMATION_MESSAGE);
+                        }
+                        break;
+                    case 4:
+                        JOptionPane.showMessageDialog(null, "Aplicacion Finalizada",
+                                "Fin", JOptionPane.INFORMATION_MESSAGE);
+                        break;
 
+                    default:
+                        JOptionPane.showMessageDialog(null, "La opcion no esta en el mensaje",
+                                "Incorrecto", JOptionPane.INFORMATION_MESSAGE);
+
+                }
+            } catch (NumberFormatException n) {
+                JOptionPane.showMessageDialog(null, "Error" + n.getMessage());
             }
-        } catch (NumberFormatException n){
-            JOptionPane.showMessageDialog(null, "Error" + n.getMessage());
-        }
-    } while (opcion != 7);
+        } while (opcion != 4);
+    }
 }
